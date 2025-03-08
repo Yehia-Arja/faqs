08214084f862db9554f6137b961433237aa02078
Yehia Arja <yehia.al.arja12@gmail.com>
2025-03-08 12:12:32 +0200

    created migrations + seeds

--- article-server/database/migrations/usersTable.php
+++ article-server/database/migrations/usersTable.php
@@ -4,11 +4,11 @@ require_once "../../connection/connection.php";
 class CreateUserTable{
     public static function up($conn) {
 
-        $sql = "CREATE TABLE IF NOT EXISTS 'users' 
-        ('id' INT AUTO_INCREMENT PRIMARY KEY,
-        'email' VARCHAR(255) NOT NULL UNIQUE,
-        'username' VARCHAR(255) NOT NULL,
-        'password' TEXT NOT NULL)";
+        $sql = "CREATE TABLE IF NOT EXISTS users 
+        (id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        username VARCHAR(255) NOT NULL,
+        password TEXT NOT NULL)";
 
         if ($conn->query($sql)) {
             echo "Table created successfully";
