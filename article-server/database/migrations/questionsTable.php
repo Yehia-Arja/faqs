9a81ea30b5d43e634fba2549512296d620e034e7
Yehia Arja <yehia.al.arja12@gmail.com>
2025-03-08 03:05:45 +0200

    added connection.php

--- article-server/database/migrations/questionsTable.php
+++ article-server/database/migrations/questionsTable.php
@@ -1,9 +1,9 @@
 <?php
-
 require_once "../../connection/connection.php";
 
 class CreateQuestionTable {
     public static function up ($conn) {
+
         $sql = "CREATE TABLE IF NOT EXISTS'questions'
         ('id' INT AUTO_INCREMENT PRIMARY KEY,
         'question' VARCHAR(255) NOT NULL,
