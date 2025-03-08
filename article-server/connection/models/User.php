d988cfaee72b860d0e6b26e6eaa24328fa16afa6
Yehia Arja <yehia.al.arja12@gmail.com>
2025-03-08 15:42:14 +0200

    added users model + skeleton

---
+++ article-server/connection/models/User.php
@@ -0,0 +1,46 @@
+<?php
+require_once "../connection.php";
+
+class User {
+    public static function addUser($user) {
+        global $conn;
+
+        $sql = $conn->prepare("INSERT INTO users (email,username,password) VALUES (?,?,?)");
+        $sql->bind_param("sss", $user->email, $user->username, $user->password);
+
+        if (!$sql->execute()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static function checkUser($user) {
+        global $conn;
+
+        $sql = $conn->prepare("SELECT * FROM users WHERE email = ?");
+        $sql->bind_param("s",$user->email);
+
+        if (!$sql->execute()) {
+            return false;
+        }
+        $result = $sql->get_result()->fetch_assoc();
+        return $result;
+
+    }
+    public static function verifyUser($user) {
+        global $conn;
+
+        $sql = $conn->prepare("SELECT * FROM users WHERE email = ?");
+        $sql->bind_param("i", $user->email);
+
+        if (!$sql->execute()) {
+            return false;
+        }
+        $result = $sql->get_result()->fetch_assoc();
+        $password_from_db = $result['password'];
+        if (!password_verify($user->password,$password_from_db)) {
+            return false;
+        }
+        return true;
+    } 
+}
